f7e5e6642ea544e3f31c8839c3aef9a9aa475b34
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-08-19 00:16:37 -0300

    Get Method

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"golang-api/controller"
+	"golang-api/db"
+	"golang-api/repository"
+	"golang-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// Camada de repository
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	// Camada usecase
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+	// Camada de controllers
+	ProductController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+
+	server.Run(":8080")
+
+}
---
+++ usecase/products_usecase.go
@@ -0,0 +1,20 @@
+package usecase
+
+import (
+	"golang-api/model"
+	"golang-api/repository"
+)
+
+type ProductUsecase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
