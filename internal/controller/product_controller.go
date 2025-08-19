4d5b8dd295d33f5de0441861fe116ee230d1f0d0
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-08-19 13:37:08 -0300

    Created POST Method.

--- internal/controller/product_controller.go
+++ internal/controller/product_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"golang-api/internal/model"
 	"golang-api/internal/usecase"
 	"net/http"
 
@@ -25,5 +26,24 @@ func (p *productController) GetProducts(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, products)
+
+}
+
+func (p *productController) CreateProduct(ctx *gin.Context) {
+
+	var product model.Product
+	err := ctx.BindJSON(&product)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := p.productUsecase.CreateProduct(product)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
 
 }
--- internal/repository/product_repository.go
+++ internal/repository/product_repository.go
@@ -46,3 +46,22 @@ func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
 	return productList, nil
 
 }
+
+func (pr *ProductRepository) CreateProduct(product model.Product) (int, error) {
+
+	var id int
+	query, err := pr.connection.Prepare("INSERT INTO product (product_name, price) VALUES ($1, $2) RETURNING ID")
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	err = query.QueryRow(product.Name, product.Price).Scan(&id)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	query.Close()
+	return id, nil
+}
